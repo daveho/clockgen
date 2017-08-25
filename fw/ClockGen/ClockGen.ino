8ab5dedf069449d6178646332b426324b40af6c9
David Hovemeyer <david.hovemeyer@gmail.com>
2017-08-25 18:56:58 -0400

    started firmware

---
+++ fw/ClockGen/ClockGen.ino
@@ -0,0 +1,26 @@
+#include <SPI.h>
+#include <Wire.h>
+#include <Adafruit_GFX.h>
+#include <Adafruit_SSD1306.h>
+
+#define OLED_RESET 4
+Adafruit_SSD1306 display(OLED_RESET);
+
+void setup() {
+  Serial.begin(9600);
+  display.begin(SSD1306_SWITCHCAPVCC, 0x3C);  // initialize with the I2C addr 0x3D (for the 128x64)
+
+  display.clearDisplay();
+
+  display.setTextSize(1);
+  display.setTextColor(WHITE);
+  display.setCursor(0,0);
+  display.println("Hello, world!");
+
+  display.display();
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+
+}
